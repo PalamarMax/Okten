4c50d216defa8276944b8b517bb0edf82e84cb6a
PalamarMax <palamarmax@gmail.com>
2022-02-01 14:54:58 +0200

    Add Lesson1 JS

---
+++ JS/Lesson1/Homework/script.js
@@ -0,0 +1,177 @@
+// ЗАВДАННЯ 1
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 1;
+let e = 10;
+let f = -999;
+let g = 123;
+let h = 3.14;
+let i = 2.7;
+let j = 16;
+let k = true;
+let l = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+
+document.write(a + "</br>");
+document.write(b + "</br>");
+document.write(c + "</br>");
+document.write(d + "</br>");
+document.write(e + "</br>");
+document.write(f + "</br>");
+document.write(g + "</br>");
+document.write(h + "</br>");
+document.write(i + "</br>");
+document.write(j + "</br>");
+document.write(k + "</br>");
+document.write(l + "</br>");
+
+
+// ЗАВДАННЯ2
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+a = 555;
+b = true;
+c = 'owu';
+d = -5;
+e = false;
+f = 3.14;
+g = 'G';
+h = 0.15;
+i = 'web';
+j = 32500;
+k = 'Katrin';
+l = 1111;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+
+document.write("</br>" + a + "</br>");
+document.write(b + "</br>");
+document.write(c + "</br>");
+document.write(d + "</br>");
+document.write(e + "</br>");
+document.write(f + "</br>");
+document.write(g + "</br>");
+document.write(h + "</br>");
+document.write(i + "</br>");
+document.write(j + "</br>");
+document.write(k + "</br>");
+document.write(l + "</br>");
+
+
+// ЗАВДАННЯ3
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName = 'Palamar';
+// let lastName = 'Maks';
+// let middleName = 'Vitaliyovish'
+// let person = (firstName + lastName + middleName);
+// console.log(person);
+
+
+// ЗАВДАННЯ4
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let name = prompt('Як тебе звати?');
+// let poBatkovi = prompt('Як тебе по-батькові?');
+// let age = prompt('Скільки тобі років?');
+// alert('Вітаю ' + name + ' ' + poBatkovi + '. Тобі ' + age +' років.');
+
+
+// ЗАВДАННЯ5
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+// ЗАВДАННЯ6
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(5 === 6);
+// console.log(10 === 10);
+// console.log(10 == 10);
+// console.log(10 > 10);
+// console.log(10 < 10);
+// console.log(10 != 10);
+// console.log(123 != '123');
+// console.log(123 == '123');
+
+
+// ЗАВДАННЯ7
+// Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+//При додаванні усі змінні рахуються як string, при інших діях як число
+
+
